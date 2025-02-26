167850be2e97d7f49eaae20914a416c3a8ff2cbf
jhyang22 <jaeho9229@gmail.com>
2025-02-26 18:24:59 +0900

    과제파일 생성

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+    }
+}
