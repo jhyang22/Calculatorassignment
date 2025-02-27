53f173194f558fd38eb0c9d2c509977fc8585d1d
jhyang22 <jaeho9229@gmail.com>
2025-02-27 15:56:36 +0900

    기본 구성 완료:

--- src/calculator/nextlevel/AppWithClass.java
+++ src/calculator/nextlevel/AppWithClass.java
@@ -8,9 +8,16 @@ public class AppWithClass {
         System.out.println(" ");
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
         int input1 = scanner.nextInt();
+        System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
         int input2 = scanner.nextInt();
 
+        Calculator calculator = new Calculator(input1, input2);
+        calculator.add(input1, input2);
+        calculator.subtract(input1, input2);
+        calculator.multiply(input1, input2);
+        calculator.divide(input1, input2);
 
 
     }
