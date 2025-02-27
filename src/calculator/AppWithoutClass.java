3f0cf132442b0926760d7599dcde6bb38713fe1b
jhyang22 <jaeho9229@gmail.com>
2025-02-27 12:39:21 +0900

    클래스명 변경

--- src/calculator/AppWithoutClass.java
+++ src/calculator/AppWithoutClass.java
@@ -2,7 +2,7 @@ package calculator;
 
 import java.util.Scanner;
 
-public class CalculatorWithoutClass {
+public class AppWithoutClass {
     public static void main(String[] args) {
         System.out.println("Hello, Calculator!");
         System.out.println(" ");
