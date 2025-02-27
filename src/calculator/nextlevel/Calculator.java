f43f26a58051cab4aa130518df59ceb6cb7ea1d9
jhyang22 <jaeho9229@gmail.com>
2025-02-27 15:58:52 +0900

    진짜 기본 설정 완료

--- src/calculator/nextlevel/Calculator.java
+++ src/calculator/nextlevel/Calculator.java
@@ -2,8 +2,8 @@ package calculator.nextlevel;
 
 public class Calculator {
     // 속성
-    int number1;
-    int number2;
+    private int number1;
+    private int number2;
 
     // 생성자
     Calculator(int number1, int number2) {
@@ -14,30 +14,34 @@ public class Calculator {
 
 
     // 기능
+
+    // 더하기
     void add(int number1, int number2) {
         int result = number1 + number2;
         System.out.println("덧셈의 결과는 " + result + "입니다.");
     }
 
+    // 빼기
     void subtract(int number1, int number2) {
         int result = number1 - number2;
         System.out.println("뺄셈의 결과는 " + result + "입니다.");
     }
 
+    // 곱하기
     void multiply(int number1, int number2) {
         int result = number1 * number2;
         System.out.println("곱셈의 결과는 " + result + "입니다.");
     }
-
+    // 나누기
     void divide(int number1, int number2) {
         if (number2 == 0) {
             System.out.println("분모가 0이면 나눗셈이 불가능합니다.");
         } else {
             int result = number1 / number2;
             int result1 = number1 % number2;
-
             System.out.println("나눗셈의 결과는 " + result + "이고, 나머지는 " + result1 + "입니다.");
         }
     }
+
 
 }
