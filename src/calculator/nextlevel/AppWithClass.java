972b3655d86df477461db9380996dc8f9bea858b
jhyang22 <jaeho9229@gmail.com>
2025-03-03 21:46:30 +0900

    전체 반복까지는 완료

--- src/calculator/nextlevel/AppWithClass.java
+++ src/calculator/nextlevel/AppWithClass.java
@@ -7,47 +7,102 @@ public class AppWithClass {
         System.out.println("Hello, Calculator!");
         System.out.println(" ");
 
-//        for (int i = 1; i > 0; i++) {
-//            // 입력
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
-//            int input1 = scanner.nextInt();
-//            System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
-//            int input2 = scanner.nextInt();
-//            Calculator calculator = new Calculator(input1, input2);
-//
-//            // 계산
-//            calculator.operator(input1, input2);
-//
-//            System.out.print("계산을 계속하시겠습니까? (y/n): ");
-//            String answer = scanner.next();
-//            for (int j = 1; j > 0; j++) {
-//                if (answer.equals("y")) {
-//                    System.out.println("계산을 계속합니다.");
-//                    break;
-//                } else if (!(answer.equals("n") || answer.equals("exit"))) {
-//                    System.out.println("잘못 입력하였습니다.\n계산을 계속하시겠습니까? (y/n): ");
-//                    answer = scanner.next();
-//                } else {
-//                    break;
-//                }
-//            }
-//            if (answer.equals("n") || answer.equals("exit")) {
-//                System.out.println("계산기를 종료합니다.");
-//                break;
-//            }
-//        }
+        while (true) {
+            // 정수 입력
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
+            int number1 = scanner.nextInt();
+            System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
+            int number2 = scanner.nextInt();
+
+            Calculator cal = new Calculator();
 
 
-        // 다시 짜보자잇
-//        Scanner scanner = new Scanner(System.in);
-//
+            // 입력 정수 확인
+            while (true) {
+                cal.setInputNum(number1, number2);
+                boolean checkNum = cal.checkNum();
+                if (checkNum) {
+                    break;
+                } else {
+                    System.out.println("0을 포함한 양의 정수가 아닙니다! 0을 포함한 양의 정수를 입력해주세요");
+                    System.out.print("첫 번째 정수를 다시 입력해주세요(0을 포함한 양의 정수): ");
+                    number1 = scanner.nextInt();
+                    System.out.print("두 번째 정수를 다시 입력해주세요(0을 포함한 양의 정수): ");
+                    number2 = scanner.nextInt();
+                }
+            }
 
+            System.out.println("현재 입력된 값은 " + number1 + ", " + number2 + "입니다");
 
-//        Calculator calculator = new Calculator();
-        Calculator.setNumber();
-//        System.out.println(Calculator.add(int number1, int number2));
+            // 사칙연산 기호 입력
+            System.out.print("사칙연산 기호를 입력해주세요 (+, -, *, /): ");
+            String sign = scanner.next();
 
+            // 입력받은 기호 확인
+            while (true) {
+                cal.setInputSign(sign);
+                boolean checkStr = cal.checkStr();
+                if (checkStr) {
+                    break;
+                } else {
+                    System.out.print("입력이 올바르지 않습니다\n사칙연산 기호를 제대로 입력해주세요 (+, -, *, /): ");
+                    sign = scanner.next();
+                }
+            }
+
+            // 사칙연산 수행
+            int result = 0;
+            int result1 = 0;
+            switch (sign) {
+                case "+":
+                    result = cal.add(number1, number2);
+                    System.out.println(number1 + " + " + number2 + " = " + result);
+                    break;
+                case "-":
+                    result = cal.sub(number1, number2);
+                    System.out.println(number1 + " - " + number2 + " = " + result);
+                    break;
+                case "*":
+                    result = cal.mul(number1, number2);
+                    System.out.println(number1 + " * " + number2 + " = " + result);
+                    break;
+                case "/":
+                    while (true) {
+                        if (number2 == 0) {
+                            System.out.print("분모는 0이 될 수 없습니다. 다시 입력해 주세요: ");
+                            number2 = scanner.nextInt();
+                        } else {
+                            cal.setInputNum(number1, number2);
+                            break;
+                        }
+                    }
+                    result = cal.div(number1, number2);
+                    result1 = cal.mod(number1, number2);
+                    System.out.println(number1 + " / " + number2 + " = " + result);
+                    System.out.println(number1 + " % " + number2 + " = " + result1);
+                    break;
+                default:
+            }
+
+            System.out.print("계산을 계속하시겠습니까? (y/n) ");
+            String answer = scanner.next();
+            while (true) {
+                if (answer.equals("y") || answer.equals("n") || answer.equals("exit")) {
+                    break;
+                } else {
+                    System.out.print("입력이 올바르지 않습니다. 다시 입력해주세요. (y/n) ");
+                    answer = scanner.next();
+                }
+            }
+
+            if (answer.equals("n") || answer.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            } else if (answer.equals("y")) {
+                System.out.println("계산을 계속합니다.");
+            }
+        }
     }
 }
 
--- src/calculator/nextlevel/Calculator.java
+++ src/calculator/nextlevel/Calculator.java
@@ -4,144 +4,78 @@ import java.util.Scanner;
 
 public class Calculator {
     // 속성
-    private static int number1;
-    private static int number2;
+    private int number1;
+    private int number2;
+    private String sign;
     private int result;
-//    private final int result;
 
-    // 생성자
-    Calculator(int number1, int number2) {
-        this.number1 = number1;
-        this.number2 = number2;
-    }
-
-
-    // 기능
     Scanner scanner = new Scanner(System.in);
 
-    // 모든 기능을 operator로 묶어서 해볼까..?
-//    int operator(int input1, int input2) {
-//        for (int i = 1; i > 0; i++) {
-//            if (input1 < 0 || input2 < 0) {
-//                System.out.println("0을 포함한 양의 정수만 입력해주세요. ");
-//                System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
-//                input1 = scanner.nextInt();
-//                System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
-//                input2 = scanner.nextInt();
-//            }
-//        }
-//        // 사칙연산 입력
-//        System.out.print("사칙연산 기호를 입력해주세요 (+, -, *, /): ");
-//        String input3 = scanner.next();
-//        for (int i = 1; i > 0; i++) {
-//            if (input3.equals("+") || input3.equals("-") || input3.equals("*") || input3.equals("/")) {
-//                switch (input3) {
-//                    case "+":
-//                        result = number1 + number2;
-////                        System.out.println("덧셈의 결과는 " + result + "입니다.");
-//                        break;
-//                    case "-":
-//                        result = number1 - number2;
-////                        System.out.println("뺄셈의 결과는 " + result + "입니다.");
-//                        break;
-//                    case "*":
-//                        result = number1 * number2;
-////                        System.out.println("곱셈의 결과는 " + result + "입니다.");
-//                        break;
-//                    case "/":
-//                        if (number2 == 0) {
-////                            System.out.println("분모가 0이면 나눗셈이 불가능합니다.");
-//                        } else {
-//                            result = number1 / number2;
+    // 생성자
+    Calculator() {
+        this.number1 = number1;
+        this.number2 = number2;
 
-    /// /                            int result = number1 % number2;
-    /// /                            System.out.println("나눗셈의 결과는 " + result + "이고, 나머지는 " + result + "입니다.");
-//                        }
-//                        break;
-//                    default:
-//                }
-//                break;
-//            } else {
-//                System.out.print("잘못 입력하였습니다\n사칙연산 기호를 제대로 입력해주세요 (+, -, *, /): ");
-//                input3 = scanner.next();
-//            }
-//        }
-//        return result;
-//    }
+    }
 
-
-    // 다시 짜보자 괜찮아
+    // 기능
     int add(int number1, int number2) {
-        this.result = number1 + number2;
-        return this.result;
-    }
-
-    int sub(int number1, int number2) {
-        this.result = number1 - number2;
-        return this.result;
-    }
-
-    int mul(int number1, int number2) {
-        this.result = number1 * number2;
-        return this.result;
-    }
-
-//    int div(int number1, int number2) {
-//        if (number2 == 0) {
-//            System.out.print("분모 자리에 0이 올 수 없습니다.");
-//            return error;
-//        } else {
-//            this.result = number1 / number2;
-//            return this.result;
-//        }
-//    }
-
-
-    // getter
-    public int getResult() {
+        result = this.number1 + this.number2;
         return result;
     }
 
-    // setter
-//    public void setNumber(int number1, int number2) {
-//        for (int i = 1; i > 0; i++) {
-//            if (number1 >= 0 && number2 >= 0) {
-//                this.number1 = number1;
-//                this.number2 = number2;
+    int sub(int number1, int number2) {
+        result = this.number1 - this.number2;
+        return result;
+    }
+
+    int mul(int number1, int number2) {
+        result = this.number1 * this.number2;
+        return result;
+    }
+
+    int div(int number1, int number2) {
+//        while (true) {
+//            if (number2 == 0) {
+//                System.out.print("분모는 0이 될 수 없습니다. 다시 입력해 주세요: ");
+//                number2 = scanner.nextInt();
 //            } else {
-//                System.out.print("잘못 입력하였습니다.\n0을 포함한 양의 정수를 입력해주세요!");
-//
+//                setInputNum(number1, number2);
+//                break;
 //            }
 //        }
-    public static void setNumber() {
-//        Calculator calculator = new Calculator(number1, number2);
-        System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
-        int number1 = scanner.nextInt();
-        System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
-        int number2 = scanner.nextInt();
-        Calculator calculator = new Calculator(number1, number2);
-        calculator.setNumber();
-        for (int i = 1; i > 0; i++) {
-            if (number1 < 0 || number2 < 0) {
-                System.out.println("잘못 입력하였습니다.\n0을 포함한 양의 정수만 입력해주세요. ");
-                System.out.print("첫 번째 정수를 입력해주세요 (0을 포함): ");
-                number1 = scanner.nextInt();
-                System.out.print("두 번째 정수를 입력해주세요 (0을 포함): ");
-                number2 = scanner.nextInt();
-            }
-        }
+        result = this.number1 / this.number2;
+        return result;
+    }
+
+    int mod(int number1, int number2) {
+        result = this.number1 % this.number2;
+        return result;
+    }
+
+    void setInputNum(int number1, int number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
 
+    boolean checkNum() {
+        if (number1 >= 0 && number2 >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void setInputSign(String sign) {
+        this.sign = sign;
+    }
+
+    boolean checkStr() {
+        if (sign.equals("+") || sign.equals("-") || sign.equals("*") || sign.equals("/")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
-
-// return 받아야 하면 void 지워야하고 흐ㅡㅁ...
-
-// 잠깐만.. setter에서 조건을 걸 수 있으면 정수 입력 부분에 만들었던 for문은 죽여도 되는거 아닌가?
-
-// 1. scanner를 Calculator 클래스에 넣고 main에서는 함수 호출만 하고 싶어
-// 2. 그러려면 main에서는 객체 생성 없이 함수를 호출해야해
-//   -> 객체 생성 없이 클래스명으로 호출하려면 static 변수가 필요해
-// 3. setNumber에도 static 선언해주고 Calculator 클래스의 number 변수에도 static 설정해줬는데 왜 안되지...
